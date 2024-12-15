f62836f33210b6d17a98a8f616729db14f3e8564
ARYAN <itszaryan593@gmail.com>
2024-12-15 16:36:44 +0600

    Update handleReply.js

--- includes/handle/handleReply.js
+++ includes/handle/handleReply.js
@@ -1,4 +1,4 @@
-module.exports = function ({ api, models, Users, Threads, Currencies }) {
+jmodule.exports = function ({ api, models, Users, Threads, Currencies }) {
     return function ({ event }) {
         if (!event.messageReply) return;
         const { handleReply, commands } = global.client
